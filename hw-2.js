ec8f4ea49a5672ff5f9707c3dd397dd6d4e15642
ZakhTania <tatizarova@gmail.com>
2023-08-25 16:29:50 +0300

    Final v

--- hw-2.js
+++ hw-2.js
@@ -1,58 +1,58 @@
 
-// // Задание 1 
-// let a = 20;
-// alert(a);
+// Задание 1 
+let x = 20;
+alert(x);
 
-// // Задание 2
-// let yearFirstIphone = 2007;
-// alert(`Год выпуска первого iPhone ${yearFirstIphone}`);
+// Задание 2
+let yearFirstIphone = 2007;
+alert(`Год выпуска первого iPhone ${yearFirstIphone}`);
 
-// // Задание 3
-// let name = 'Brendan Eich'
-// alert(name);
+// Задание 3
+let creatorsName = 'Brendan Eich'
+alert(creatorsName);
 
-// // Задание 4
-// let a = 10;
-// let b = 2;
-// alert(`a + b = ${a + b};
-// a - b = ${a - b};
-// a * b = ${a * b};
-// a / b = ${a / b}.`);
+// Задание 4
+let firstNum = 10;
+let secondNum = 2;
+alert(`a + b = ${firstNum + secondNum};
+a - b = ${firstNum - secondNum};
+a * b = ${firstNum * secondNum};
+a / b = ${firstNum / secondNum}.`);
 
-// // Задание 5
-// let result = 2 ** 5;
-// alert(result);
+// Задание 5
+let result = 2 ** 5;
+alert(result);
 
-// // Задание 6
-// let a = 9;
-// let b = 2;
-// alert(a % b);
+// Задание 6
+let a = 9;
+let b = 2;
+alert(a % b);
 
-// // Задание 7
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num += 1;
-// num -= 1;
-// alert(num);
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
 
-// // Задание 8
-// let age = prompt(`Сколько вам лет?`);
-// alert(age);
+// Задание 8
+let age = prompt(`Сколько вам лет?`);
+alert(age);
 
-// // Задание 9
-// let user = {
-//     name: 'Tanya',
-//     age: 34,
-//     isAdmin: false
-// };
-// user['city of residence'] = 'Orel';
-// user.age = 35;
-// delete user['city of residence'];
-// let info = prompt(`Какую информацию хотите узнать о пользователе?`);
-// alert(user[info]);
+// Задание 9
+let user = {
+    name: 'Tanya',
+    age: 34,
+    isAdmin: false
+};
+user['city of residence'] = 'Orel';
+user.age = 35;
+delete user['city of residence'];
+let info = prompt(`Какую информацию хотите узнать о пользователе?`);
+alert(user[info]);
 
 // Задание 10
 let userName = prompt(`Как вас зовут?`);
