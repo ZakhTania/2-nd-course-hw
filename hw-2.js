df8b6e14025d528b6ba2d0fa117e6a053ef9d2fa
ZakhTania <tatizarova@gmail.com>
2023-08-25 16:11:43 +0300

    Д/з 2.2

--- hw-2.js
+++ hw-2.js
@@ -19,6 +19,43 @@
 // a * b = ${a * b};
 // a / b = ${a / b}.`);
 
-// Задание 5
-let result = 2 ** 5;
-alert(result);
+// // Задание 5
+// let result = 2 ** 5;
+// alert(result);
+
+// // Задание 6
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+// // Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+// // Задание 8
+// let age = prompt(`Сколько вам лет?`);
+// alert(age);
+
+// // Задание 9
+// let user = {
+//     name: 'Tanya',
+//     age: 34,
+//     isAdmin: false
+// };
+// user['city of residence'] = 'Orel';
+// user.age = 35;
+// delete user['city of residence'];
+// let info = prompt(`Какую информацию хотите узнать о пользователе?`);
+// alert(user[info]);
+
+// Задание 10
+let userName = prompt(`Как вас зовут?`);
+alert(`Привет, ${userName}!`);
+
+
