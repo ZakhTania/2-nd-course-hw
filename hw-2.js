2c5e8aa3baa7b97267b11ab9cfd43918fd54d859
ZakhTania <tatizarova@gmail.com>
2023-08-25 15:09:45 +0300

    Задание 5

--- hw-2.js
+++ hw-2.js
@@ -11,10 +11,14 @@
 // let name = 'Brendan Eich'
 // alert(name);
 
-// Задание 4
-let a = 10;
-let b = 2;
-alert(`a + b = ${a + b};
-a - b = ${a - b};
-a * b = ${a * b};
-a / b = ${a / b}.`);
+// // Задание 4
+// let a = 10;
+// let b = 2;
+// alert(`a + b = ${a + b};
+// a - b = ${a - b};
+// a * b = ${a * b};
+// a / b = ${a / b}.`);
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
