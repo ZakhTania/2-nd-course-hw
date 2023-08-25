e9a617c7edb92e9b0af78127b011b0b15c7de048
ZakhTania <tatizarova@gmail.com>
2023-08-25 14:58:44 +0300

    Задание 3

--- hw-2.js
+++ hw-2.js
@@ -3,6 +3,10 @@
 // let a = 20;
 // alert(a);
 
-// Задание 2
-let yearFirstIphone = 2007;
-alert(`Год выпуска первого iPhone ${yearFirstIphone}`);
+// // Задание 2
+// let yearFirstIphone = 2007;
+// alert(`Год выпуска первого iPhone ${yearFirstIphone}`);
+
+// Задание 3
+let name = 'Brendan Eich'
+alert(name);
