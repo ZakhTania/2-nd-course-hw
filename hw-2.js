4bcaa9d0041ae30934a911b0f1c45ad19c8e9733
ZakhTania <tatizarova@gmail.com>
2023-08-25 14:53:40 +0300

    Задание 2

--- hw-2.js
+++ hw-2.js
@@ -1,4 +1,8 @@
 
-// Задание 1 
-let a = 20;
-alert(a);
+// // Задание 1 
+// let a = 20;
+// alert(a);
+
+// Задание 2
+let yearFirstIphone = 2007;
+alert(`Год выпуска первого iPhone ${yearFirstIphone}`);
