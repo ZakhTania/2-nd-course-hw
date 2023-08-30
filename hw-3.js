526f29cbdd4c09eea58858d76f0f48b205824fad
ZakhTania <tatizarova@gmail.com>
2023-08-30 14:54:39 +0300

    Добавила scss файл для мобильной версии

--- hw-3.js
+++ hw-3.js
@@ -29,31 +29,33 @@
 // let b = '3';
 // alert(Number(a) + Number(b));
 
-// Задание 5
-let monthNumber = Number(prompt(`Введите номер месяца`));
-if (monthNumber > 0 && monthNumber < 13) {
-    switch (monthNumber) {
-        case 1:
-        case 2:
-        case 12:
-            console.log(`Зима`);
-            break;
-        case 3:
-        case 4:
-        case 5:
-            console.log(`Весна`);
-            break;
-        case 6:
-        case 7:
-        case 8:
-            console.log(`Лето`);
-            break;
-        case 9:
-        case 10:
-        case 11:
-            console.log(`Осень`);
-            break;
-        default:
-            break;
-    }
-};
+// // Задание 5
+// let monthNumber = Number(prompt(`Введите номер месяца`));
+// if (monthNumber > 0 && monthNumber < 13) {
+//     switch (monthNumber) {
+//         case 1:
+//         case 2:
+//         case 12:
+//             console.log(`Зима`);
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             console.log(`Весна`);
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             console.log(`Лето`);
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             console.log(`Осень`);
+//             break;
+//         default:
+//             break;
+//     }
+// };
+
+// Задание 7
