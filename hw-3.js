175c0ea8280743eee155e0f82443a56bc45ebc0c
ZakhTania <tatizarova@gmail.com>
2023-08-30 13:47:33 +0300

    Задание 1

--- hw-3.js
+++ hw-3.js
@@ -1,2 +1,9 @@
-
+// Задание 1
+let password = 'пароль';
+let verification = prompt(`Введите пароль`);
+if (password === verification) {
+    alert(`Пароль введен верно`);
+} else {
+    alert(`Пароль введен неправильно`);
+};
 
