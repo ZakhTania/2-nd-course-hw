2b7b0970025cc9f968bab53bd3f684b6a9ee2401
ZakhTania <tatizarova@gmail.com>
2023-08-30 14:28:15 +0300

    Задание 5

--- hw-3.js
+++ hw-3.js
@@ -24,7 +24,36 @@
 //     console.log(`Неверно`);
 // };
 
-// Задание 4
-let a = '2';
-let b = '3';
-alert(Number(a) + Number(b));
+// // Задание 4
+// let a = '2';
+// let b = '3';
+// alert(Number(a) + Number(b));
+
+// Задание 5
+let monthNumber = Number(prompt(`Введите номер месяца`));
+if (monthNumber > 0 && monthNumber < 13) {
+    switch (monthNumber) {
+        case 1:
+        case 2:
+        case 12:
+            console.log(`Зима`);
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log(`Весна`);
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log(`Лето`);
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log(`Осень`);
+            break;
+        default:
+            break;
+    }
+};
