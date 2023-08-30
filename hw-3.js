7eaa18b640ab41062a63d67ade4a6574fbe254e9
ZakhTania <tatizarova@gmail.com>
2023-08-30 14:03:42 +0300

    Задание 4

--- hw-3.js
+++ hw-3.js
@@ -15,11 +15,16 @@
 //     console.log(`Неверно`);
 // };
 
-// Задание 3
-let d = 45;
-let e = 101;
-if (d > 100 || e > 100) {
-    console.log(`Верно`);
-} else {
-    console.log(`Неверно`);
-};
+// // Задание 3
+// let d = 45;
+// let e = 101;
+// if (d > 100 || e > 100) {
+//     console.log(`Верно`);
+// } else {
+//     console.log(`Неверно`);
+// };
+
+// Задание 4
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
