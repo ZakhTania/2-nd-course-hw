a45e78e4ee8c443e0ed77252c8e938e4fc0ee33c
ZakhTania <tatizarova@gmail.com>
2023-08-30 13:55:57 +0300

    Задание 2

--- hw-3.js
+++ hw-3.js
@@ -1,9 +1,16 @@
-// Задание 1
-let password = 'пароль';
-let verification = prompt(`Введите пароль`);
-if (password === verification) {
-    alert(`Пароль введен верно`);
-} else {
-    alert(`Пароль введен неправильно`);
-};
+// // Задание 1
+// let password = 'пароль';
+// let verification = prompt(`Введите пароль`);
+// if (password === verification) {
+//     alert(`Пароль введен верно`);
+// } else {
+//     alert(`Пароль введен неправильно`);
+// };
 
+// Задание 2
+let c = Number(prompt(`Запишите любое число`));
+if (c > 0 && c < 10) {
+    console.log(`Верно`);
+} else {
+    console.log(`Неверно`);
+};
