d808c411f86852333a7445b6c4aec3305fb5144a
ZakhTania <tatizarova@gmail.com>
2023-09-02 16:35:28 +0300

    задание 6

--- hw-4.js
+++ hw-4.js
@@ -1,28 +1,28 @@
-// // Задание 1
-// for (let i = 0; i < 2; i++) {
-//     console.log(`Привет`);
-// }
+// Задание 1
+for (let i = 0; i < 2; i++) {
+    console.log(`Привет`);
+}
 
-// // Задание 2
-// for (let number = 1; number < 6; number++) {
-//    console.log(number);
-// 1   
-// }
+// Задание 2
+for (let number = 1; number < 6; number++) {
+   console.log(number);
+1   
+}
 
-// // Задание 3
-// for (let count = 7; count < 23; count++){
-// console.log(count);
-// }
+// Задание 3
+for (let count = 7; count < 23; count++){
+console.log(count);
+}
 
-// // Задание 4
-// const obj = {
-//     Коля: `200`,
-//     Вася: `300`,
-//     Петя: `400`
-// }
-// for (let name in obj) {
-//     console.log(`${name} -  зарплата ${obj[name]} долларов`);
-// }
+// Задание 4
+const obj = {
+    Коля: `200`,
+    Вася: `300`,
+    Петя: `400`
+}
+for (let name in obj) {
+    console.log(`${name} -  зарплата ${obj[name]} долларов`);
+}
 
 // Задание 5
 let num = 0;
@@ -32,3 +32,8 @@ do {
     num++
 } while (n > 50);
 console.log(`Получившееся число ${n}, необходимое количество итераций ${num}`);
+
+// Задание 6
+for (let friday = 4; friday <=31; friday += 7) {
+    console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+}
