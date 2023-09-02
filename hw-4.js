d7df44b3c640ce1e19027a6c36f49d7d97472031
ZakhTania <tatizarova@gmail.com>
2023-09-02 15:30:34 +0300

    Задание 2

--- hw-4.js
+++ hw-4.js
@@ -1,4 +1,10 @@
-// Задание 1
-for (let i = 0; i < 2; i++) {
-    console.log(`Привет`);
+// // Задание 1
+// for (let i = 0; i < 2; i++) {
+//     console.log(`Привет`);
+// }
+
+// Задание 2
+for (let number = 1; number < 6; number++) {
+   console.log(number);
+1   
 }
