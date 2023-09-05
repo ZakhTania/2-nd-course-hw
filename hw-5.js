e5e492a18f25af2ce920ea9ac368a12e71d42b08
ZakhTania <tatizarova@gmail.com>
2023-09-05 15:14:47 +0300

    Сверстала блок seasons

--- hw-5.js
+++ hw-5.js
@@ -1,68 +1,68 @@
-// // Задание 1
-// let maxNumber = (firstNumber, secondNumber) => {
-//     let max = firstNumber > secondNumber ? firstNumber : secondNumber;
-//     return max;
-// }
-// console.log(maxNumber(Number(prompt(`Введите первое число`)), Number(prompt(`Введите второе число`))));
+// Задание 1
+let maxNumber = (firstNumber, secondNumber) => {
+    let max = firstNumber > secondNumber ? firstNumber : secondNumber;
+    return max;
+}
+console.log(maxNumber(Number(prompt(`Введите первое число`)), Number(prompt(`Введите второе число`))));
 
-// // Задание 2
-// let num;
-// let evenOrodd = (num) => {
-//     num = num % 2 === 0 ? `Число четное` : 'Число нечетное';
-//     return num;
-// }
-// console.log(evenOrodd(8));
+// Задание 2
+let num;
+let evenOrodd = (num) => {
+    num = num % 2 === 0 ? `Число четное` : 'Число нечетное';
+    return num;
+}
+console.log(evenOrodd(8));
 
-// // Задание 3
-// // 3.1.
-// let squareOfnum = (anyNumber) => {
-//     let sqr = anyNumber * anyNumber;
-//     console.log(`Квадрат чила ${anyNumber} равен ${sqr}`);
-// }
-// squareOfnum(8);
+// Задание 3
+// 3.1.
+let squareOfnum = (anyNumber) => {
+    let sqr = anyNumber * anyNumber;
+    console.log(`Квадрат чила ${anyNumber} равен ${sqr}`);
+}
+squareOfnum(8);
 
-// // 3.2.
-// let sqrFnmbr = (nmbr) => nmbr *nmbr;
+// 3.2.
+let sqrFnmbr = (nmbr) => nmbr *nmbr;
 
-// // Задание 4
-// let greeting = () => {
-//     let howOld = Number(prompt(`Сколько Вам лет?`));
-//     if (howOld < 0) {
-//         howOld = `Вы ввели неправильное значение`;
-//     } else {
-//         howOld = howOld < 13 ? `Привет, друг!` : `Добро пожаловать!`;
-//     }
-//     alert(howOld);
-// }
-// console.log(greeting());
+// Задание 4
+let greeting = () => {
+    let howOld = Number(prompt(`Сколько Вам лет?`));
+    if (howOld < 0) {
+        howOld = `Вы ввели неправильное значение`;
+    } else {
+        howOld = howOld < 13 ? `Привет, друг!` : `Добро пожаловать!`;
+    }
+    alert(howOld);
+}
+console.log(greeting());
 
-// // Задание 5
-// let multTwoNum = () => {
-//     let a = prompt(`Введите первое число`);
-//     let b = prompt(`Введите второе число`);
-//     let result;
-//     if (!isNaN(a) && !isNaN(b)) {
-//         result = a * b;
-//     } else {
-//         result = 'Одно или оба значения не являются числом';
-//     }
-//     return result;
-// }
-// console.log(multTwoNum());
+// Задание 5
+let multTwoNum = () => {
+    let a = prompt(`Введите первое число`);
+    let b = prompt(`Введите второе число`);
+    let result;
+    if (!isNaN(a) && !isNaN(b)) {
+        result = a * b;
+    } else {
+        result = 'Одно или оба значения не являются числом';
+    }
+    return result;
+}
+console.log(multTwoNum());
 
-// // Задание 6
-// let n = prompt(`Введите число`);
-// let result;
-// let cubicN = () => {
-//     if (!isNaN(n) && n != '' && n != null) {
-//         let cube = n * n * n;
-//         result = `${n} в кубе равняется ${cube}`
-//     } else {
-//         result = `Переданный параметр не является числом`;
-//     }
-//     return result;
-// }
-// console.log(cubicN());
+// Задание 6
+let n = prompt(`Введите число`);
+let result;
+let cubicN = () => {
+    if (!isNaN(n) && n != '' && n != null) {
+        let cube = n * n * n;
+        result = `${n} в кубе равняется ${cube}`
+    } else {
+        result = `Переданный параметр не является числом`;
+    }
+    return result;
+}
+console.log(cubicN());
 
 // Задание 7
 function getArea() {
@@ -84,4 +84,3 @@ const circle2 = {
     getArea: getArea,
     getPerimeter: getPerimeter,
 }
-
