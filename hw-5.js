9eb3e98ca26b2a0448716b6b2e2c18ff5380f2ad
ZakhTania <tatizarova@gmail.com>
2023-09-05 17:54:31 +0300

    Задание 8

--- hw-5.js
+++ hw-5.js
@@ -23,6 +23,7 @@ squareOfnum(8);
 
 // 3.2.
 let sqrFnmbr = (nmbr) => nmbr *nmbr;
+sqrFnmbr(5);
 
 // Задание 4
 let greeting = () => {
@@ -34,7 +35,6 @@ let greeting = () => {
     }
     alert(howOld);
 }
-console.log(greeting());
 
 // Задание 5
 let multTwoNum = () => {
@@ -83,4 +83,29 @@ const circle2 = {
     radius: 5,
     getArea: getArea,
     getPerimeter: getPerimeter,
+}
+
+// Задание 8
+let seasons = () => {
+    let month;
+    let output;
+    let play;
+    do {
+        month = Number(prompt(`Введите номер месяца (от 1 до 12)`))
+        if (month > 0 && month < 13) {
+            if (month === 1 || month === 2 || month === 12) {
+                output = `Зима`;
+            } else if (month === 3 || month === 4 || month === 5) {
+                output = `Весна`;
+            } else if (month === 6 || month === 7 || month === 8) {
+                output = `Лето`;
+            } else if (month === 9 || month === 10 || month === 11) {
+                output = `Осень`;
+            }
+        } else {
+            output = `Неверно введен номер месяца`
+        }
+        alert(output);
+        play = confirm(`Повторить?`)
+    } while (play === true);
 }
