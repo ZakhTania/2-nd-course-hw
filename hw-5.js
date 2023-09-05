9e933693fa126095c80f61779bc08124b630dd12
ZakhTania <tatizarova@gmail.com>
2023-09-05 13:02:39 +0300

    Задание 2

--- hw-5.js
+++ hw-5.js
@@ -1,6 +1,17 @@
-// Задание 1
-let maxNumber = (firstNumber, secondNumber) => {
-    let max = firstNumber > secondNumber ? firstNumber : secondNumber;
-    return max;
+// // Задание 1
+// let maxNumber = (firstNumber, secondNumber) => {
+//     let max = firstNumber > secondNumber ? firstNumber : secondNumber;
+//     return max;
+// }
+// console.log(maxNumber(Number(prompt(`Введите первое число`)), Number(prompt(`Введите второе число`))));
+
+// Задание 2
+let num;
+let evenOrodd = () => {
+    do {
+        num = prompt(`Введите число`);
+    } while (isNaN(num));
+    num = Number(num) % 2 === 0 ? `Число четное` : 'Число нечетное';
+    return num;
 }
-console.log(maxNumber(Number(prompt(`Введите первое число`)), Number(prompt(`Введите второе число`))));
+console.log(evenOrodd());
