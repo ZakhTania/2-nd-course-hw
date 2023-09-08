eaeb8338203dd76955e90b0c2c00ebcfd1558546
ZakhTania <tatizarova@gmail.com>
2023-09-08 15:59:44 +0300

    Задание 1

---
+++ hw-6.js
@@ -0,0 +1,8 @@
+// Задание 1
+const arrayOfnumber = [1, 5, 4, 10, 0, 3];
+for (let el = 0; el < arrayOfnumber.length; el++) {
+    console.log(arrayOfnumber[el]);
+    if (arrayOfnumber[el] === 10) {
+        break;
+    }
+}
