b404df34a825f7910dc2a741e4cc0b3b8ac6a194
ZakhTania <tatizarova@gmail.com>
2023-09-08 16:30:26 +0300

    Задание 3

--- hw-6.js
+++ hw-6.js
@@ -1,7 +1,9 @@
 // // Задание 1
 const arrayOfnumber = [1, 5, 4, 10, 0, 3];
+
 for (let el = 0; el < arrayOfnumber.length; el++) {
     console.log(arrayOfnumber[el]);
+
     if (arrayOfnumber[el] === 10) {
         break;
     }
@@ -9,3 +11,8 @@ for (let el = 0; el < arrayOfnumber.length; el++) {
 
 // Задание 2
 console.log(`Позиция числа 4 в данном массиве -  [${arrayOfnumber.indexOf(4)}].`);
+
+// Задание 3
+const anyArray = [1, 3, 5, 10, 20];
+
+console.log(anyArray.join([' ']));
