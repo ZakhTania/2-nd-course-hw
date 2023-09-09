38ade9c8e40402c5af91bf65c7b4c5c2f9209710
ZakhTania <tatizarova@gmail.com>
2023-09-09 09:07:32 +0300

    Задание 15

--- hw-6.js
+++ hw-6.js
@@ -1,136 +1,136 @@
-// // // Задание 1
-// const arrayOfnumber = [1, 5, 4, 10, 0, 3];
+// // Задание 1
+const arrayOfnumber = [1, 5, 4, 10, 0, 3];
 
-// for (let item of arrayOfnumber) {
-//     console.log(item);
+for (let item of arrayOfnumber) {
+    console.log(item);
 
-//     if (item === 10) {
-//         break;
-//     }
-// }
+    if (item === 10) {
+        break;
+    }
+}
 
-// // Задание 2
-// console.log(`Позиция числа 4 в данном массиве -  [${arrayOfnumber.indexOf(4)}].`);
+// Задание 2
+console.log(`Позиция числа 4 в данном массиве -  [${arrayOfnumber.indexOf(4)}].`);
 
-// // Задание 3
-// const anyArray = [1, 3, 5, 10, 20];
+// Задание 3
+const anyArray = [1, 3, 5, 10, 20];
 
-// console.log(anyArray.join([' ']));
+console.log(anyArray.join([' ']));
 
-// // Задание 4
-// let multiArr = [];
-// let simpleArr = [];
+// Задание 4
+let multiArr = [];
+let simpleArr = [];
 
-// while (multiArr.length < 3) {
+while (multiArr.length < 3) {
 
-//     while (simpleArr.length < 3) {
-//         simpleArr.push(1);
-//     }
+    while (simpleArr.length < 3) {
+        simpleArr.push(1);
+    }
 
-//     multiArr.push(simpleArr);
-// }
-// console.log(multiArr);
+    multiArr.push(simpleArr);
+}
+console.log(multiArr);
 
-// // Задание 5
-// const ar = [1, 1, 1];
+// Задание 5
+const ar = [1, 1, 1];
 
-// while (ar.length < 6) {
+while (ar.length < 6) {
 
-//     ar.push(2);
-// }
+    ar.push(2);
+}
 
-// console.log(ar);
+console.log(ar);
 
-// // Задание 6
-// const unsortArray = [9, 8, 7, 'a', 6, 5];
+// Задание 6
+const unsortArray = [9, 8, 7, 'a', 6, 5];
 
-// unsortArray.sort();
+unsortArray.sort();
 
-// unsortArray.pop()
+unsortArray.pop()
 
-// console.log(unsortArray);
+console.log(unsortArray);
 
-// //  Задание 7
+//  Задание 7
 
-// let numb = Number(prompt(`Введите цифру от 1 до 9`));
-// let answ;
+let numb = Number(prompt(`Введите цифру от 1 до 9`));
+let answ;
 
-// for (let i = 0; i < unsortArray.length; i++) {
+for (let i = 0; i < unsortArray.length; i++) {
 
-//     if (unsortArray[i] == numb) {
-//         answ = `Угадал`;
-//         break;
+    if (unsortArray[i] == numb) {
+        answ = `Угадал`;
+        break;
 
-//     } else {
-//         answ = `Не угадал`;
-//     }
-// }
+    } else {
+        answ = `Не угадал`;
+    }
+}
 
-// alert(answ);
+alert(answ);
 
-// // Задание 8
-// const str = 'abcdef';
+// Задание 8
+const str = 'abcdef';
 
-// let strArr = str.split('');
+let strArr = str.split('');
 
-// strArr = strArr.reverse();
+strArr = strArr.reverse();
 
-// console.log(strArr.join(''));
+console.log(strArr.join(''));
 
-// // Задание 9
-// const arrayNum = [[1, 2, 3,], [4, 5, 6]];
-// let nextArr = [];
+// Задание 9
+const arrayNum = [[1, 2, 3,], [4, 5, 6]];
+let nextArr = [];
 
-// nextArr = nextArr.concat(arrayNum[0], arrayNum[1]);
+nextArr = nextArr.concat(arrayNum[0], arrayNum[1]);
 
-// console.log(nextArr);
+console.log(nextArr);
 
-// //  Задание 10
-// const sumArr = [2, 4, 8, 1, 6, 5, 9];
+//  Задание 10
+const sumArr = [2, 4, 8, 1, 6, 5, 9];
 
-// for (let each = 0; each < sumArr.length - 1; each++) {
+for (let each = 0; each < sumArr.length - 1; each++) {
 
-//     console.log(sumArr[each] + sumArr[each + 1]);
-// }
+    console.log(sumArr[each] + sumArr[each + 1]);
+}
 
-// // Задание 11
-// console.log(sumArr);
+// Задание 11
+console.log(sumArr);
 
-// const newSumArr = sumArr.map(el => el * el);
+const newSumArr = sumArr.map(el => el * el);
 
-// console.log(newSumArr);
+console.log(newSumArr);
 
-// // Задание 12
-// let outArray = [];
+// Задание 12
+let outArray = [];
 
-// const getLengthWords = (inArray) => {
+const getLengthWords = (inArray) => {
 
-//     for (let i = 0; i < inArray.length; i++) {
+    for (let i = 0; i < inArray.length; i++) {
 
-//         outArray[i] = inArray[i].length;
+        outArray[i] = inArray[i].length;
 
-//     }
-// 
-//     return outArray;
-// }
+    }
 
-// console.log(getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']));
+    return outArray;
+}
 
-// // Задание 13
-// function filterPositive(array) {
-// let arrayAfter = [];
+console.log(getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']));
 
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] < 0) {
-//             arrayAfter.push(array[i]);
-//         }
-//     }
+// Задание 13
+function filterPositive(array) {
+let arrayAfter = [];
 
-//     return arrayAfter;
-// }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            arrayAfter.push(array[i]);
+        }
+    }
 
-// console.log(filterPositive([-1, 0, 5, -10, 56]));
-// console.log(filterPositive([-25, 25, 0, -1000, -2]));
+    return arrayAfter;
+}
+
+console.log(filterPositive([-1, 0, 5, -10, 56]));
+console.log(filterPositive([-25, 25, 0, -1000, -2]));
 
 // Задание 14
 const randomArr = [];
@@ -151,3 +151,18 @@ for (let el = 0; el < randomArr.length; el++) {
 
 console.log(randomArr);
 console.log(randomEvenArr);
+
+// Задание 15
+const randomSix = [];
+let sum = 0;
+
+for (let i = 0; i < 6; i++) {
+    randomSix[i] = Math.floor(Math.random() * 10) + 1;
+ 
+    sum += randomSix[i];
+}
+
+let average = sum / randomSix.length;
+
+console.log(randomSix);
+console.log(average);
