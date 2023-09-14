b7070de20efb9f3abec25d4912ea84d2f7178c63
ZakhTania <tatizarova@gmail.com>
2023-09-14 11:53:56 +0300

    Задание 3

--- hw-7.js
+++ hw-7.js
@@ -3,12 +3,18 @@
 // stringJs = stringJs.toUpperCase();
 // console.log(stringJs);
 
-// Задание 2
-function searchStart(arrayStrings, str) {
-    let strArrayFilter = arrayStrings.filter((arrayString) => arrayString.substring(0, str.length).toLowerCase() === str.toLowerCase());
-    return strArrayFilter;
+// // Задание 2
+// function searchStart(arrayStrings, str) {
+//     let strArrayFilter = arrayStrings.filter((arrayString) => arrayString.substring(0, str.length).toLowerCase() === str.toLowerCase());
+//     return strArrayFilter;
     
-}
-console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
-console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
-console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+// }
+// console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+// console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+// console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+
+// Задание 3
+const number = 32.58884;
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
