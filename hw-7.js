69837542f3e133c39c4657fcf2b2e2b2e6beccd7
ZakhTania <tatizarova@gmail.com>
2023-09-14 11:48:38 +0300

    Задание 2

--- hw-7.js
+++ hw-7.js
@@ -1,4 +1,14 @@
-// Задание 1
-let stringJs = 'какая-то строка';
-stringJs = stringJs.toUpperCase();
-console.log(stringJs);
+// // Задание 1
+// let stringJs = 'какая-то строка';
+// stringJs = stringJs.toUpperCase();
+// console.log(stringJs);
+
+// Задание 2
+function searchStart(arrayStrings, str) {
+    let strArrayFilter = arrayStrings.filter((arrayString) => arrayString.substring(0, str.length).toLowerCase() === str.toLowerCase());
+    return strArrayFilter;
+    
+}
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
