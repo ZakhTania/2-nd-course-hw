0462f7a5d8f84c5edb6aea32f38656eb6d69ae27
ZakhTania <tatizarova@gmail.com>
2023-09-16 22:25:14 +0300

    Задание 2

--- hw-8.js
+++ hw-8.js
@@ -1,14 +1,46 @@
 // Задание 1
 const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
- ];
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
 
- const sortByField = (fieldName) => {
-    return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
- }
+const sortByField = (fieldName) => {
+   return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+}
 
- console.log(people.sort(sortByField('age')));
- 
+console.log(people.sort(sortByField('age')));
+
+//   Задание 2
+function isPositive(el) {
+
+   return el > 0 ? true : false;
+}
+function isMale(i) {
+
+   return i['gender'] === 'male' ? true : false;
+}
+function filter(arr, ruleFunction) {
+   const output = [];
+
+   for (let i = 0; i < arr.length; i++) {
+      
+      if (ruleFunction(arr[i])) {
+         output.push(arr[i]);
+      }
+   }
+
+   return output;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+
+const peopleGender = [
+   { name: 'Глеб', gender: 'male' },
+   { name: 'Анна', gender: 'female' },
+   { name: 'Олег', gender: 'male' },
+   { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(peopleGender, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
